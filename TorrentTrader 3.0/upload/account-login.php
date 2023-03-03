a4f403ddfde7e8a61c1f83de83585643ccca7807
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2023-03-03 15:14:33 -0500

    sql security update

--- TorrentTrader 3.0/upload/account-login.php
+++ TorrentTrader 3.0/upload/account-login.php
@@ -1,72 +1,100 @@
 <?php
+// Updated login for sql security March 2023
 require_once("backend/functions.php");
 dbconn();
 session_start();
 
 if (!empty($_REQUEST["returnto"])) {
-	if (!$_GET["nowarn"]) {    
-		 $nowarn = T_("MEMBERS_ONLY");
-	}
+    if (!isset($_GET["nowarn"])) {
+        $nowarn = T_("MEMBERS_ONLY");
+    }
 }
 
-if ($_POST["username"] && $_POST["password"]) {
-	$password = passhash($_POST["password"]);
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $password = passhash($_POST["password"]);
 
-	if (!empty($_POST["username"]) && !empty($_POST["password"])) {
-		$res = SQL_Query_exec("SELECT id, password, secret, status, enabled FROM users WHERE username = " . sqlesc($_POST["username"]) . "");
-		$row = mysqli_fetch_assoc($res);
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+        $stmt = $GLOBALS["DBconnector"]->prepare("SELECT id, password, secret, status, enabled FROM users WHERE username = ?");
+        $stmt->bind_param("s", $_POST["username"]);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $row = $res->fetch_assoc();
 
-		if ( ! $row || $row["password"] != $password )
-			$message = T_("LOGIN_INCORRECT");
-		elseif ($row["status"] == "pending")
-			$message = T_("ACCOUNT_PENDING");
-		elseif ($row["enabled"] == "no")
-			$message = T_("ACCOUNT_DISABLED");
-	} else
-		$message = T_("NO_EMPTY_FIELDS");
+        if (!$row || ($_POST["password"], $row["password"])) {
+            $message = T_("LOGIN_INCORRECT");
+        } elseif ($row["status"] == "pending") {
+            $message = T_("ACCOUNT_PENDING");
+        } elseif ($row["enabled"] == "no") {
+            $message = T_("ACCOUNT_DISABLED");
+        }
+    } else {
+        $message = T_("NO_EMPTY_FIELDS");
+    }
 
-	if (!$message){
-		logincookie($row["id"], $row["password"], $row["secret"]);
-		if (!empty($_POST["returnto"])) {
-			header("Refresh: 0; url=" . $_POST["returnto"]);
-			die();
-		}
-		else {
-			header("Refresh: 0; url=index.php");
-			die();
-		}
-	}else{
-		show_error_msg(T_("ACCESS_DENIED"), $message, 1);
-	}
+    if (!$message) {
+        logincookie($row["id"], $row["password"], $row["secret"]);
+        if (!empty($_POST["returnto"])) {
+            header("Refresh: 0; url=" . $_POST["returnto"]);
+            die();
+        } else {
+            header("Refresh: 0; url=index.php");
+            die();
+        }
+    } else {
+        show_error_msg(T_("ACCESS_DENIED"), $message, 1);
+    }
 }
 
 logoutcookie();
 
 stdhead(T_("LOGIN"));
- 
- if ($nowarn)
-      show_error_msg(T_("ERROR"), $nowarn, 0);
-      
-begin_frame(T_("LOGIN"));
 
+if (isset($nowarn)) {
+    show_error_msg(T_("ERROR"), $nowarn, 0);
+}
+
+
+begin_frame(T_("LOGIN"));
 ?>
 
 <form method="post" action="account-login.php">
-  <table border="0" cellpadding="3" align="center">
-		<tr><td align="center"><b><?php echo T_("USERNAME"); ?>:</b> <input type="text" size="40" name="username" /></td></tr>
-		<tr><td align="center"><b><?php echo T_("PASSWORD"); ?>:</b> <input type="password" size="40" name="password" /></td></tr>
-		<tr><td colspan="2" align="center"><input type="submit" value="<?php echo T_("LOGIN"); ?>" /><br /><br /><i><?php echo T_("COOKIES");?></i></td></tr>
-	</table>
-<?php
+    <table border="0" cellpadding="3" align="center">
+        <tr>
+            <td align="center">
+                <b><?php echo T_("USERNAME"); ?>:</b>
+                <input type="text" size="40" name="username" />
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <b><?php echo T_("PASSWORD"); ?>:</b>
+                <input type="password" size="40" name="password" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit" value="<?php echo T_("LOGIN"); ?>" />
+                <br />
+                <br />
+                <i><?php echo T_("COOKIES");?></i>
+            </td>
+        </tr>
+    </table>
 
-if ( ! empty($_REQUEST["returnto"]) )
-{ 
-      print("<input type=\"hidden\" name=\"returnto\" value=\"" . cleanstr($_REQUEST["returnto"]) . "\" />\n");
-}
-?>
+    <?php
+    if (!empty($_REQUEST["returnto"])) {
+        ?>
+        <input type="hidden" name="returnto" value="<?php echo cleanstr($_REQUEST["returnto"]); ?>" />
+        <?php
+    }
+    ?>
 
 </form>
-<p align="center"><a href="account-signup.php"><?php echo T_("SIGNUP"); ?></a> | <a href="account-recover.php"><?php echo T_("RECOVER_ACCOUNT"); ?></a></p>
+
+<p align="center">
+    <a href="account-signup.php"><?php echo T_("SIGNUP"); ?></a> |
+    <a href="account-recover.php"><?php echo T_("RECOVER_ACCOUNT"); ?></a>
+</p>
 
 <?php
 end_frame();
