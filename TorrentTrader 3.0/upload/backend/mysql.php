478dfd06604827dcacc401d1ee7c438089b99104
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2024-12-09 14:11:41 -0500

    Update mysql.php

--- TorrentTrader 3.0/upload/backend/mysql.php
+++ TorrentTrader 3.0/upload/backend/mysql.php
@@ -1,14 +1,13 @@
 <?php
-//MYSQL CONNECTION INFO, DONT PASS IT OUT!
-
-//Access Security check
-if (basename($_SERVER['PHP_SELF']) === 'mysql.php') {
-    die;
+// Prevent direct access to this script
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    exit;
 }
 
-//Change the settings below to match your MYSQL server connection settings
-$mysql_host = "localhost";  //leave this as localhost if you are unsure
-$mysql_user = "tt";  //Username to connect
-$mysql_pass = "tt"; //Password to connect
-$mysql_db = "tt";  //Database name
+// Change the settings below to match your MySQL/MariaDB server connection settings
+$mysql_host = getenv('MYSQL_HOST') ?: 'localhost'; // Default to 'localhost'
+$mysql_user = getenv('MYSQL_USER') ?: 'tt';        // Username to connect
+$mysql_pass = getenv('MYSQL_PASS') ?: 'tt';        // Password to connect
+$mysql_db = getenv('MYSQL_DB') ?: 'tt';            // Database name
+
 ?>
