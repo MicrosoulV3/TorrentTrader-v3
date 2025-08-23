2e92087a2a267b03979ccc52f8c4e2c66d9ce27b
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2025-08-23 15:27:33 -0400

    Update mysql.class.php

--- TorrentTrader 3.0/upload/backend/mysql.class.php
+++ TorrentTrader 3.0/upload/backend/mysql.class.php
@@ -1,101 +1,200 @@
 <?php
-
-
-function &SQL_Query ($query) {
-	return new SQL_Query ($query);
+function SQL_Query(string $query) {
+    return new SQL_Query($query);
 }
 
-function &SQL_Query_exec ($query) {
-	$sql = new SQL_Query ($query);
-	return $sql->execute();
+function SQL_Query_exec(string $query) {
+    try {
+        $sql = new SQL_Query($query);
+        return $sql->execute();
+    } catch (Exception $e) {
+        error_log("SQL Query Execution Error: " . $e->getMessage());
+        return null;
+    }
 }
 
 class SQL_Query {
-	private $query = "";
-	private $params = array();
+    private string $query = "";
+    private array $params = [];
 
-	function &__construct ($query) {
-		$this->query = $query;
-		return $this;
-	}
+    public function __construct(string $query) {
+        $this->query = $query;
+        return $this;
+    }
 
-	function &p ($param) {
-		if (is_numeric($param)) {
-			$this->params[] = $param;
-		} elseif (is_array($param)) {
-			$this->params[] = implode(", ", array_map(array(&$this, "escape"), $param));
-		} else {
-			$this->params[] = $this->escape($param);
-		}
-		return $this;
-	}
+    public function p($param) {
+        if (is_numeric($param)) {
+            $this->params[] = $param;
+        } elseif (is_array($param)) {
+            $this->params[] = implode(", ", array_map([$this, "escape"], $param));
+        } else {
+            $this->params[] = $this->escape($param);
+        }
+        return $this;
+    }
 
-	function &p_name ($param) {
-		$this->params[] = "`".mysqli_real_escape_string($GLOBALS["DBconnector"],$param)."`";
-		return $this;
-	}
+    public function p_name(string $param) {
+        $this->params[] = "`" . mysqli_real_escape_string($GLOBALS["DBconnector"], $param) . "`";
+        return $this;
+    }
 
-	function escape ($s) {
-		if (is_numeric($s))
-			return $s;
-		return "'".mysqli_real_escape_string($GLOBALS["DBconnector"],$s)."'";
-	}
+    public function escape($s) {
+        if (is_numeric($s)) {
+            return $s;
+        }
+        return "'" . mysqli_real_escape_string($GLOBALS["DBconnector"], $s) . "'";
+    }
 
-	function read () {
-		$ret = "";
-		if (count($this->params)) {
-			reset($this->params);
-			for ($i = 0; $i < strlen($this->query); $i++) {
-				if ($this->query[$i] == "?") {
-				//	list(, $val) = thisEach($this->params);
-					$val = current($this->params); //new var to replace thisEach
-					next($this->params); //new var to replace thisEach
-					$ret .= $val;
-				} else {
-					$ret .= $this->query[$i];
-				}
-			}
-			reset($this->params);
-		} else {
-			$ret = $this->query;
-		}
-		return $ret;
-	}
+    public function read() {
+        $ret = "";
+        if (count($this->params)) {
+            reset($this->params);
+            for ($i = 0; $i < strlen($this->query); $i++) {
+                if ($this->query[$i] == "?") {
+                    $val = current($this->params);
+                    next($this->params);
+                    $ret .= $val;
+                } else {
+                    $ret .= $this->query[$i];
+                }
+            }
+            reset($this->params);
+        } else {
+            $ret = $this->query;
+        }
+        return $ret;
+    }
 
-	function &execute () {
-		$query = $this->read();
-		$res = mysqli_query($GLOBALS["DBconnector"],$query);
+    public function execute() {
+        $query = $this->read();
+        $res = mysqli_query($GLOBALS["DBconnector"], $query);
 
-		if ($res || mysqli_errno($GLOBALS["DBconnector"]) == 1062) {
-			return $res;
-		}
-		$mysqli_error = mysqli_error($GLOBALS["DBconnector"]);
-		$mysqli_errno = mysqli_errno($GLOBALS["DBconnector"]);
+        if ($res || mysqli_errno($GLOBALS["DBconnector"]) == 1062) {
+            return $res;
+        }
+        $mysqli_error = mysqli_error($GLOBALS["DBconnector"]);
+        $mysqli_errno = mysqli_errno($GLOBALS["DBconnector"]);
 
-		// If debug_backtrace() is available, we can find exactly where the query was called from
-		if (function_exists("debug_backtrace")) {
-			$bt = debug_backtrace();
+        if (function_exists("debug_backtrace")) {
+            $bt = debug_backtrace();
+            $i = 1;
+            if ($bt[$i]["function"] == "SQL_Query_exec_cached" || $bt[$i]["function"] == "get_row_count_cached" || $bt[$i]["function"] == "get_row_count") {
+                $i++;
+            }
+            $line = $bt[$i]["line"];
+            $file = str_replace(getcwd() . DIRECTORY_SEPARATOR, "", $bt[$i]["file"]);
+            $msg = "Database Error in $file on line $line: $mysqli_error. Query was: $query.";
+        } else {
+            $file = str_replace(getcwd() . DIRECTORY_SEPARATOR, "", $_SERVER["SCRIPT_FILENAME"]);
+            $msg = "Database Error in $file: $mysqli_error. Query was: $query";
+        }
 
-			$i = 1;
+        mysqli_query($GLOBALS["DBconnector"], "INSERT INTO `sqlerr` (`txt`, `time`) 
+                     VALUES (" . sqlesc($msg) . ", '" . get_date_time() . "')");
 
-			if ($bt[$i]["function"] == "SQL_Query_exec_cached" || $bt[$i]["function"] == "get_row_count_cached" || $bt[$i]["function"] == "get_row_count")
-				$i++;
+        show_error_msg("Database Error", "Database Error. Please report this to an administrator.", 1);
 
-			$line = $bt[$i]["line"];
-			$file = str_replace(getcwd().DIRECTORY_SEPARATOR, "", $bt[$i]["file"]);
-			$msg = "Database Error in $file on line $line: $mysqli_error. Query was: $query.";
-		} else {
-			$file = str_replace(getcwd().DIRECTORY_SEPARATOR, "", $_SERVER["SCRIPT_FILENAME"]);
-			$msg = "Database Error in $file: $mysqli_error. Query was: $query";
-		}
-        
-        mysqli_query($GLOBALS["DBconnector"],"INSERT INTO `sqlerr` (`txt`, `time`) 
-                     VALUES (".sqlesc($msg).", '".get_date_time()."')");
-
-        if ( function_exists('show_error_msg') )
-		     show_error_msg("Database Error", "Database Error. Please report this to an administrator.", 1);
-	}
+        return null;
+    }
 }
+
+// OLD CODE COMMENTED BELOW FOR REFERENCE
+
+// function &SQL_Query ($query) {
+// 	return new SQL_Query ($query);
+// }
+
+// function &SQL_Query_exec ($query) {
+// 	$sql = new SQL_Query ($query);
+// 	return $sql->execute();
+// }
+
+// class SQL_Query {
+// 	private $query = "";
+// 	private $params = array();
+
+// 	function &__construct ($query) {
+// 		$this->query = $query;
+// 		return $this;
+// 	}
+
+// 	function &p ($param) {
+// 		if (is_numeric($param)) {
+// 			$this->params[] = $param;
+// 		} elseif (is_array($param)) {
+// 			$this->params[] = implode(", ", array_map(array(&$this, "escape"), $param));
+// 		} else {
+// 			$this->params[] = $this->escape($param);
+// 		}
+// 		return $this;
+// 	}
+
+// 	function &p_name ($param) {
+// 		$this->params[] = "`".mysqli_real_escape_string($GLOBALS["DBconnector"],$param)."`";
+// 		return $this;
+// 	}
+
+// 	function escape ($s) {
+// 		if (is_numeric($s))
+// 			return $s;
+// 		return "'".mysqli_real_escape_string($GLOBALS["DBconnector"],$s)."'";
+// 	}
+
+// 	function read () {
+// 		$ret = "";
+// 		if (count($this->params)) {
+// 			reset($this->params);
+// 			for ($i = 0; $i < strlen($this->query); $i++) {
+// 				if ($this->query[$i] == "?") {
+// 				//	list(, $val) = thisEach($this->params);
+// 					$val = current($this->params); //new var to replace thisEach
+// 					next($this->params); //new var to replace thisEach
+// 					$ret .= $val;
+// 				} else {
+// 					$ret .= $this->query[$i];
+// 				}
+// 			}
+// 			reset($this->params);
+// 		} else {
+// 			$ret = $this->query;
+// 		}
+// 		return $ret;
+// 	}
+
+// 	function &execute () {
+// 		$query = $this->read();
+// 		$res = mysqli_query($GLOBALS["DBconnector"],$query);
+
+// 		if ($res || mysqli_errno($GLOBALS["DBconnector"]) == 1062) {
+// 			return $res;
+// 		}
+// 		$mysqli_error = mysqli_error($GLOBALS["DBconnector"]);
+// 		$mysqli_errno = mysqli_errno($GLOBALS["DBconnector"]);
+
+// 		// If debug_backtrace() is available, we can find exactly where the query was called from
+// 		if (function_exists("debug_backtrace")) {
+// 			$bt = debug_backtrace();
+
+// 			$i = 1;
+
+// 			if ($bt[$i]["function"] == "SQL_Query_exec_cached" || $bt[$i]["function"] == "get_row_count_cached" || $bt[$i]["function"] == "get_row_count")
+// 				$i++;
+
+// 			$line = $bt[$i]["line"];
+// 			$file = str_replace(getcwd().DIRECTORY_SEPARATOR, "", $bt[$i]["file"]);
+// 			$msg = "Database Error in $file on line $line: $mysqli_error. Query was: $query.";
+// 		} else {
+// 			$file = str_replace(getcwd().DIRECTORY_SEPARATOR, "", $_SERVER["SCRIPT_FILENAME"]);
+// 			$msg = "Database Error in $file: $mysqli_error. Query was: $query";
+// 		}
+        
+//         mysqli_query($GLOBALS["DBconnector"],"INSERT INTO `sqlerr` (`txt`, `time`) 
+//                      VALUES (".sqlesc($msg).", '".get_date_time()."')");
+
+//         if ( function_exists('show_error_msg') )
+// 		     show_error_msg("Database Error", "Database Error. Please report this to an administrator.", 1);
+// 	}
+// }
 
 /* Example Usage:
 
